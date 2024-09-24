c3a5ae0652722058331da0c9e00431af2738d304
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-24 05:49:47 +0000

    Populates Multiplication Table Printer

---
+++ MrJones_Sprint_Projectv1.5/MrJones_Sprint_Projectv1.5.py
@@ -0,0 +1,7 @@
+def multi_table(a):
+    for i in range(1, 11):
+        print ('{0} x {1} = {2}'.format(a, i, a*i))
+
+if __name__ == '__main__':
+    a = input('Please enter a number here: ')
+    multi_table((float(a))
