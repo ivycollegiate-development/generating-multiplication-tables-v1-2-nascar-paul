2f0673acc8edef8df0db2fa4b51d23c00ccc64c8
Paul Jones <paul.tyler.jones@gmail.com>
2024-09-24 06:16:03 +0000

    Updates v1.6 with code from 1.5

--- MrJones_Sprint_Projectv1.5/MrJones_Sprint_Projectv1.6.py
+++ MrJones_Sprint_Projectv1.5/MrJones_Sprint_Projectv1.6.py
@@ -1,4 +1,11 @@
+def multi_table(a):
+    for i in range(1, 11):
+        print ('{0} x {1} = {2}'.format(a, i, a*i))
 
+if __name__ == '__main__':
+    a = input('Please enter a number here: ')
+    multi_table(float(a))
+    
 def main():
     while True:
         try:
